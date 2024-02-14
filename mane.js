336c76c0def7b5a1ae475f2d23d0fe4fca5ff51a
YuliiaBakun <31136767+YuliiaBakun@users.noreply.github.com>
2024-02-14 22:29:53 +0200

    add files

---
+++ mane.js
@@ -0,0 +1,28 @@
+const burger = document.querySelector('.burger');
+
+const nav = document.querySelector('.nav');
+console.log(burger);
+
+// burger.addEventListener('click', () => {
+//     nav.classList.toggle('show');
+// })
+burger.addEventListener('click', () => {
+    burger.classList.toggle('close')
+})
+
+
+$('.decor-slider').slick({
+    prevArrow: '<button type="button" class="decor-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z"/></svg></button>',
+    nextArrow: '<button type="button" class="decor-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"/></svg></i></button>',
+    dots: true,
+    // dotsClass: 'decor-dots'
+    autoplay: true,
+    fade: true,
+    // infinite: false,
+
+}
+)
+
+
+// const burger = document.querySelector('.burger');
+// console.log(burger)
